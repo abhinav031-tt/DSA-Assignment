b00c3349da02fa4c08eb3c49a97de4c77dbc7c7f
abhinav031-tt <113112279+abhinav031-tt@users.noreply.github.com>
2022-10-28 23:34:54 +0530

    Update Assignment_B.py

---
+++ Assignment_B.py
@@ -0,0 +1,19 @@
+Question :- Q2. Write a program to reverse an array in place? 
+In place means you cannot create a new array. You have to update the original array.'''
+
+
+#Solution :- 
+
+array_=[1,2,3,4,5,6,7,8,9]
+array_.reverse()
+print(array_)
+
+#Output:- [9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+
+
+#withinput
+array_=list(input("Enter a value :- "))
+array_.reverse()
+print(array_)
